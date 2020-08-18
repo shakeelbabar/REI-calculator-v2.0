ffbaed161ccd6e120400878840945a1d9946d530
Shakeel Ahmed <shakeel.jamali47@gmail.com>
2020-08-18 23:25:23 +0500

    Project Created

---
+++ REI-Calculator/src/rei/calculator/REICalculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rei.calculator;
+
+/**
+ *
+ * @author Shakeel Ahmed
+ */
+public class REICalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
